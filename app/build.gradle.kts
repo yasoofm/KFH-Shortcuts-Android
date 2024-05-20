45c65811a705d2576188edd0c1be7300b7b672ab
Amar <ajk.86@hotmail.com>
2024-05-20 14:55:18 +0300

    Network Manager

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,9 +50,10 @@ android {
 }
 
 dependencies {
-
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("androidx.core:core-ktx:1.13.1")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.0")
     implementation("androidx.activity:activity-compose:1.8.2")
     implementation(platform("androidx.compose:compose-bom:2023.08.00"))
     implementation("androidx.compose.ui:ui")
