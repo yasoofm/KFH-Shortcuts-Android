180c158466925520ca224fc34984435a79bab5bc
83336 <halshamlan@yahoo.com>
2024-05-27 12:23:57 +0300

    i finished the design of the history screen

--- app/src/main/java/com/example/kfh_shortcuts/composable/RewardsScreen.kt
+++ app/src/main/java/com/example/kfh_shortcuts/composable/RewardsScreen.kt
@@ -1,18 +1,268 @@
 package com.example.kfh_shortcuts.composable
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+
 @Composable
 fun RewardsScreen() {
-    Box(
-        modifier = Modifier.fillMaxSize(),
-        contentAlignment = Alignment.Center
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color(0xFFF8F8F8))
     ) {
-        Text(text = "reward Screen", fontSize = 24.sp)
+        TopBaaaar(name = "Nawaf Almutairi", id = "83320", points = "1000000")
+        Spacer(modifier = Modifier.height(16.dp))
+        Text(
+            text = "Rewards",
+            color = Color.Gray,
+            fontSize = 18.sp,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.padding(start = 16.dp, bottom = 8.dp)
+        )
+        RewardList()
     }
 }
+
+@Composable
+fun TopBaaaar(name: String, id: String, points: String) {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(300.dp)
+            .graphicsLayer {
+                clip = true
+                shape = RoundedCornerShape(bottomStart = 29.dp, bottomEnd = 29.dp)
+            }
+            .background(
+                brush = Brush.linearGradient(
+                    colors = listOf(Color(0xFF007A3D), Color(0xFF0D4228), Color(0xFF000000)),
+                    start = Offset.Zero,
+                    end = Offset.Infinite
+                )
+            ),
+        contentAlignment = Alignment.TopStart
+    ) {
+        Column(
+            modifier = Modifier
+                .padding(16.dp)
+                .fillMaxHeight(),
+            verticalArrangement = Arrangement.SpaceBetween,
+            horizontalAlignment = Alignment.Start
+        ) {
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                Text(
+                    text = "My box",
+                    color = Color.White,
+                    fontSize = 18.sp,
+                    fontWeight = FontWeight.Bold
+                )
+            }
+            Spacer(modifier = Modifier.height(8.dp))
+            Column {
+                Text(
+                    text = "Welcome 👋",
+                    color = Color.White,
+                    fontSize = 20.sp,
+                    fontWeight = FontWeight.Bold
+                )
+                Text(
+                    text = name,
+                    color = Color.White,
+                    fontSize = 18.sp,
+                    fontWeight = FontWeight.SemiBold
+                )
+                Text(
+                    text = id,
+                    color = Color.White,
+                    fontSize = 16.sp,
+                    fontWeight = FontWeight.Medium
+                )
+            }
+            Spacer(modifier = Modifier.height(8.dp))
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(
+                    text = "total points",
+                    color = Color.White,
+                    fontSize = 20.sp,
+                    fontWeight = FontWeight.SemiBold
+                )
+                Text(
+                    text = points,
+                    color = Color.White,
+                    fontSize = 48.sp,
+                    fontWeight = FontWeight.Bold
+                )
+                Text(
+                    text = "points",
+                    color = Color.White,
+                    fontSize = 16.sp,
+                    fontWeight = FontWeight.Medium
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun RewardList() {
+    Column {
+        repeat(3) {
+            RewardItem(
+                title = "Day OFF",
+                points = "100 Points",
+                onClick = { /* Handle click */ }
+            )
+            Spacer(modifier = Modifier.height(20.dp))
+        }
+    }
+}
+
+@Composable
+fun RewardItem(title: String, points: String, onClick: () -> Unit) {
+    Card(
+        shape = RoundedCornerShape(8.dp),
+//        backgroundColor = Color.White,
+//        elevation = 4.dp,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 16.dp)
+            .clickable(onClick = onClick)
+    ) {
+        Row(
+            modifier = Modifier
+                .padding(16.dp)
+                .fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Text(
+                text = title,
+                color = Color.Black,
+                fontSize = 16.sp,
+                fontWeight = FontWeight.Bold
+            )
+            Text(
+                text = points,
+                color = Color.Gray,
+                fontSize = 16.sp,
+                fontWeight = FontWeight.Bold
+            )
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//package com.example.kfh_shortcuts.composable
+//
+//import androidx.compose.foundation.background
+//import androidx.compose.foundation.layout.Arrangement
+//import androidx.compose.foundation.layout.Box
+//import androidx.compose.foundation.layout.Column
+//import androidx.compose.foundation.layout.Spacer
+//import androidx.compose.foundation.layout.fillMaxSize
+//import androidx.compose.foundation.layout.fillMaxWidth
+//import androidx.compose.foundation.layout.height
+//import androidx.compose.foundation.layout.padding
+//import androidx.compose.foundation.shape.RoundedCornerShape
+//import androidx.compose.material3.Text
+//import androidx.compose.runtime.Composable
+//import androidx.compose.ui.Alignment
+//import androidx.compose.ui.Modifier
+//import androidx.compose.ui.geometry.Offset
+//import androidx.compose.ui.graphics.Brush
+//import androidx.compose.ui.graphics.Color
+//import androidx.compose.ui.graphics.graphicsLayer
+//import androidx.compose.ui.text.font.FontWeight
+//import androidx.compose.ui.unit.dp
+//import androidx.compose.ui.unit.sp
+//@Composable
+//fun RewardsScreen() {
+//    Column(
+//        modifier = Modifier
+//            .fillMaxSize()
+//            .background(Color(0xFFF8F8F8))
+//    ) {
+//        TopBaaaar(name = "Nawaf Almutairi", id = "83320")
+//        Spacer(modifier = Modifier.height(16.dp))
+//
+//    }
+//}
+//
+//@Composable
+//fun TopBaaaar(name: String, id: String) {
+//    Box(
+//        modifier = Modifier
+//            .fillMaxWidth()
+//            .height(300.dp)
+//            .graphicsLayer {
+//                clip = true
+//                shape = RoundedCornerShape(bottomStart = 29.dp, bottomEnd = 29.dp)
+//            }
+//            .background(
+//                brush = Brush.linearGradient(
+//                    colors = listOf(Color(0xFF007A3D), Color(0xFF0D4228), Color(0xFF000000)),
+//                    start = Offset.Zero,
+//                    end = Offset.Infinite
+//                )
+//            ),
+//        contentAlignment = Alignment.TopStart
+//    ) {
+//        Column(
+//            modifier = Modifier.padding(16.dp),
+//            verticalArrangement = Arrangement.Center
+//        ) {
+//
+//            Spacer(modifier = Modifier.height(8.dp))
+//            Text(
+//                text = "Welcome 👋",
+//                color = Color.White,
+//                fontSize = 20.sp,
+//                fontWeight = FontWeight.Bold
+//            )
+//            Text(
+//                text = name,
+//                color = Color.White,
+//                fontSize = 18.sp,
+//                fontWeight = FontWeight.SemiBold
+//            )
+//            Text(
+//                text = id,
+//                color = Color.White,
+//                fontSize = 16.sp,
+//                fontWeight = FontWeight.Medium
+//            )
+//        }
+//    }
+//}
